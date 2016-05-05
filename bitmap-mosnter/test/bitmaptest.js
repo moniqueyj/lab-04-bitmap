e6359418abf59106b3e35ca82db8f6a31271e0f0
Jacob Knaack <jacobknaack@Jacobs-MacBook-Pro-2.local>
2016-05-05 12:15:17 -0700

    added test file

---
+++ bitmap-mosnter/test/bitmaptest.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const reader = require('../lib/readbitmap');
+const expect = require('chai').expect;
+
+describe('testing the reading function', function() {
+  describe('testing output of readbitmap.js', function() {
+    it('should return a buffer of hex', function(done) {
+      reader.bitmapHeader('bitmap1.bmp', function(err, data) {
+        expect(err).to.equal(null);
+        expect(data.toString('hex', 0, 14)).to.equal('424d462b00000000000036040000');
+        done();
+      });
+    });
+
+    it('should');
+  });
+});
