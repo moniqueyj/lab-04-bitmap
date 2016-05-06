3369a0e5b72361be42101db2781b5cc2ac20bec2
Andrew Watson <andrewpeterwatson@gmail.com>
2016-05-05 19:44:34 -0700

    transfomr function

--- bitmap-mosnter/index.js
+++ bitmap-mosnter/index.js
@@ -2,14 +2,11 @@
 
 const bitmapreader = require('./lib/readbitmap');
 const bitoobject = require('./lib/parser');
+const colortransform = require('./lib/transform').colortransform;
 
-//read bitmapfile
+// read bitmapfile
 bitmapreader.bitmapHeader('bitmap1.bmp', (err, data) => {
   var newobject = new bitoobject.Buffobject(data);
   console.log(newobject);
-});
-
-//write new bitmapfile
-bitmapreader.newbitmapfile(buffer, (err, data) => {
-
+  colortransform(newobject.colors);
 });
--- bitmap-mosnter/lib/transform.js
+++ bitmap-mosnter/lib/transform.js
@@ -1,1 +1,7 @@
 //takes js bitmap & manipulate data
+'use strict';
+
+module.exports.colortransform = function(colornum) {
+  var halfnum = colornum / 2;
+  console.log('color transform: ' + halfnum);
+};
